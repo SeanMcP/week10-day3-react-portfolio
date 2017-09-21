30f347b3b44dbb64c3f1d3d6ebce130a061e27bb
Sean McPherson <seanmcp25@gmail.com>
2017-09-21 09:59:52 -0400

    Add references

--- src/components/References.js
+++ src/components/References.js
@@ -7,7 +7,25 @@ export default class References extends Component {
         <div className="card-block p-4">
           <h1 className="display-4">References</h1>
           <p className="lead">Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam viverra urna in ante sodales facilisis. Phasellus nunc arcu, interdum nec aliquet ut, ornare eu lorem. Vestibulum massa elit, consectetur vitae tristique nec, porta ultricies metus. Phasellus eget sagittis nisl. Vestibulum eget vestibulum lectus, vel consequat diam.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam viverra urna in ante sodales facilisis. Phasellus nunc arcu, interdum nec aliquet ut, ornare eu lorem.</p>
+          <hr className="my-4" />
+          <img src="https://vignette2.wikia.nocookie.net/lotr/images/e/e7/Gandalf_the_Grey.jpg/revision/latest?cb=20121110131754" className="float-right" style={{width: 120}}/>
+          <h4>Gandalf the Grey</h4>
+          <p className="lead">The Wandering Wizard</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam viverra urna in ante sodales facilisis. Phasellus nunc arcu, interdum nec aliquet ut, ornare eu lorem. Curabitur sollicitudin est rutrum, cursus orci nec, lacinia turpis. Morbi magna nulla, convallis dictum congue vitae, convallis in massa.</p>
+          <p>Donec gravida pulvinar imperdiet. Nam viverra dolor purus, vitae viverra nibh tempus ac. Morbi ut efficitur ligula, non ultricies sem. Nunc auctor pharetra mattis.</p>
+          <hr className="my-4" />
+          <img src="https://vignette2.wikia.nocookie.net/lotr/images/b/b6/Aragorn_profile.jpg" className="float-right" style={{width: 120}}/>
+          <h4>Aragorn son of Arathorn</h4>
+          <p className="lead">King of Men</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam viverra urna in ante sodales facilisis. Phasellus nunc arcu, interdum nec aliquet ut, ornare eu lorem. Curabitur sollicitudin est rutrum, cursus orci nec, lacinia turpis. Morbi magna nulla, convallis dictum congue vitae, convallis in massa.</p>
+          <p>Donec gravida pulvinar imperdiet. Nam viverra dolor purus, vitae viverra nibh tempus ac. Morbi ut efficitur ligula, non ultricies sem. Nunc auctor pharetra mattis.</p>
+          <hr className="my-4" />
+          <img src="https://i.pinimg.com/originals/df/9e/34/df9e349f2ee0832ab8bbcab971573275.png" className="float-right" style={{width: 120}}/>
+          <h4>Gimli son of Gloin</h4>
+          <p className="lead">The Dwarf</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam viverra urna in ante sodales facilisis. Phasellus nunc arcu, interdum nec aliquet ut, ornare eu lorem. Curabitur sollicitudin est rutrum, cursus orci nec, lacinia turpis. Morbi magna nulla, convallis dictum congue vitae, convallis in massa.</p>
+          <p>Donec gravida pulvinar imperdiet. Nam viverra dolor purus, vitae viverra nibh tempus ac. Morbi ut efficitur ligula, non ultricies sem. Nunc auctor pharetra mattis.</p>
         </div>
       </div>
     )
