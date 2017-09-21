19cfb73f6d84d07f1067be17457d31376739394d
Sean McPherson <seanmcp25@gmail.com>
2017-09-21 10:05:48 -0400

    Add contact links to Contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,6 +8,14 @@ export default class Contact extends Component {
           <h1 className="display-4">Contact me</h1>
           <p className="lead">Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam viverra urna in ante sodales facilisis. Phasellus nunc arcu, interdum nec aliquet ut, ornare eu lorem. Vestibulum massa elit, consectetur vitae tristique nec, porta ultricies metus. Phasellus eget sagittis nisl. Vestibulum eget vestibulum lectus, vel consequat diam.</p>
+          <h4>Get in touch</h4>
+          <ul>
+            <li><a href="mailto:mail@mail.mail">Email</a></li>
+            <li><a href="https://githubt.com">GitHub</a></li>
+            <li><a href="https://twitter.com">Twitter</a></li>
+            <li><a href="https://facebook.com">Facebook</a></li>
+            <li><a href="tel:14048675309">(404) 867-5309</a></li>
+          </ul>
         </div>
       </div>
     )
